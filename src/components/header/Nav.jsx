c9dc5ba6750e6e06f3cbd8ff4786edd575bee6fb
Michael Wilcox <webdev.krygore@gmail.com>
2024-08-14 19:39:02 -0700

    Initial Commit

---
+++ src/components/header/Nav.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+import { useState, useEffect } from 'react'
+
+const Nav = () => {
+    return ( 
+        <NavLink to='/'>Home</NavLink>
+
+     );
+}
+ 
+export default Nav;
